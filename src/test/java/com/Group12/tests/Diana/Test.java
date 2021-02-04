88dd579c4b8fc9c4906036c173eebf0ac9942f97
dianadudic <dianadudic@gmail.com>
2021-02-03 22:53:21 -0500

    * USER STORY 10: * As a user, I should be able to assign tasks by clicking on Task tab. * Logging in as a Marketing User

--- src/test/java/com/Group12/tests/Diana/Test.java
+++ src/test/java/com/Group12/tests/Diana/Test.java
@@ -1,4 +1,33 @@
 package com.Group12.tests.Diana;
 
-public class Test {
+import com.google.common.base.Verify;
+
+public class Test  {
+
+    // USER STORY:
+    // As a user, I should be able to assign tasks by clicking on Task tab.
+
+
+    // Acceptance Criteria:
+    // 1. Users can check  "High Priority"
+    // 2. Users can click on "Checklist" to create checklists items
+    // 3. Users can add Deadline, Time Planning by using date pickers. (check box practice)
+
+
+    // Scenarios:
+
+    // 1. users  (hr, marketing and helpdesk)  can check "High Priority"
+    // 2. users can click on "Checklist" to create checklists items
+    // 3. users can add deadline by selecting a date from calendar
+    // 4. users can config task starting date, duration and ending date with "Time Planning" feature
+
+
+
+
+
+
+
+
+
+
 }
