ed26b7ae19b64e6f6cfaad3c5d7f3789ad733645
volkan <vsaydogan@gmail.com>
2021-02-04 11:11:46 -0500

    TestCase_G12_125 "Verify Users can write a poll message title with question and answers"

--- src/test/java/com/Group12/tests/Volkan/Test.java
+++ src/test/java/com/Group12/tests/Volkan/Test.java
@@ -1,7 +1,5 @@
 package com.Group12.tests.Volkan;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("I will start the test cases here");
-    }
+
 }
---
+++ src/test/java/com/Group12/tests/Volkan/User_Story_4/TestCase_G12_116.java
@@ -0,0 +1,28 @@
+package com.Group12.tests.Volkan.User_Story_4;
+
+import com.Group12.TestBase.LoginToTryCrm;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+
+public class TestCase_G12_116 extends LoginToTryCrm {
+
+
+    @Test
+    public void allow_multiple_ChoiceCouldBeCheckedByUsers(){
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
