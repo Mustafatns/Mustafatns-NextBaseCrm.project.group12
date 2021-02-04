2ac2ec6343820f15429f02575cd0246cd744696c
Sevara Isroilova <sevara.i.d@gmail.com>
2021-02-03 19:12:12 -0500

    NextBase CRM

--- src/test/java/com/Group12/tests/Sevara/Test.java
+++ src/test/java/com/Group12/tests/Sevara/Test.java
@@ -3,6 +3,7 @@ package com.Group12.tests.Sevara;
 public class Test {
     public static void main(String[] args) {
         System.out.println("I will work under user story 5 ");
+        System.out.println("We decided to work on CRM Project");
 
     }
 }
