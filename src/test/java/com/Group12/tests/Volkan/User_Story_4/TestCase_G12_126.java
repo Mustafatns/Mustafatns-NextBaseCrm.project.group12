5f450c2da4332f795b89a33a10ad048ccaa31f69
volkan <vsaydogan@gmail.com>
2021-02-04 13:40:08 -0500

    TestCase_G12_116

--- src/test/java/com/Group12/tests/Volkan/User_Story_4/TestCase_G12_126.java
+++ src/test/java/com/Group12/tests/Volkan/User_Story_4/TestCase_G12_126.java
@@ -7,25 +7,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import java.awt.*;
-
-public class TestCase_G12_116 extends LoginToTryCrm {
+public class TestCase_G12_126 extends LoginToTryCrm {
 
 
     @Test
-    public void allow_multiple_ChoiceCouldBeCheckedByUsers() throws InterruptedException {
+    public void verify_Users_can_add_questions() throws InterruptedException {
 
         //button Click to Poll
         WebElement pollButton = Driver.getDriver().findElement(By.xpath("//span[@id='feed-add-post-form-tab-vote']"));
         pollButton.click();
-        BrowserUtils.sleep(1);
+        BrowserUtils.sleep(2);
 
         //Enter the title
         Driver.getDriver().switchTo().frame(Driver.getDriver().findElement(By.className("bx-editor-iframe")));
         WebElement titleBox = Driver.getDriver().findElement(By.xpath("//body"));
         titleBox.click();
-        titleBox.sendKeys("Clock2");
-        BrowserUtils.sleep(1);
+        titleBox.sendKeys("Clock");
+        BrowserUtils.sleep(2);
         Driver.getDriver().switchTo().defaultContent(); //exit the frame there is only one frame!
 
 
@@ -36,32 +34,35 @@ public class TestCase_G12_116 extends LoginToTryCrm {
 
         //fill the Answer Box 1
         WebElement answer1Box = Driver.getDriver().findElement(By.xpath("//input[@placeholder='Answer  1']"));
-        answer1Box.sendKeys("it is 11:35");
+        answer1Box.sendKeys("it is 11:34");
         BrowserUtils.sleep(1);
 
         //fill the Answer Box 2
         WebElement answer2Box = Driver.getDriver().findElement(By.xpath("//input[@placeholder='Answer  2']"));
-        answer2Box.sendKeys("it is 11:41");
+        answer2Box.sendKeys("it is 11:40");
         BrowserUtils.sleep(1);
 
         //Clicking Multiple choice button
         WebElement allowMultipleChoiceBox = Driver.getDriver().findElement(By.xpath("//input[@class='vote-checkbox']"));
         allowMultipleChoiceBox.click();
-        BrowserUtils.sleep(3);
-
-        //Click the send button
-        WebElement sendButton = Driver.getDriver().findElement(By.xpath("//button[@id='blog-submit-button-save']"));
-        sendButton.click();
         BrowserUtils.sleep(2);
 
-        ////a[@class='vote-new-question-link addq']
+        //clicking add question option
+        WebElement addQuestions = Driver.getDriver().findElement(By.xpath("//a[@class='vote-new-question-link addq']"));
+        addQuestions.click();
+        BrowserUtils.sleep(2);
+
+        //adding another question1
+        WebElement questionBox1 = Driver.getDriver().findElement(By.xpath("(//input[@placeholder='Question '])[2]"));
+        //questionBox1.click();
+        questionBox1.sendKeys("What is today?");
+        BrowserUtils.sleep(2);
 
 
-
-
-
-
-
+       //Click the send button
+        WebElement sendButton = Driver.getDriver().findElement(By.xpath("//button[@id='blog-submit-button-save']"));
+        sendButton.click();
+        BrowserUtils.sleep(4);
 
 
 
@@ -69,7 +70,6 @@ public class TestCase_G12_116 extends LoginToTryCrm {
 
 
     }
-
 
 
 
---
+++ src/test/java/com/Group12/tests/Volkan/User_Story_4/TestCase_G12_130.java
@@ -0,0 +1,24 @@
+package com.Group12.tests.Volkan.User_Story_4;
+
+import com.Group12.TestBase.LoginToTryCrm;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class TestCase_G12_130 extends LoginToTryCrm {
+
+
+    @Test
+    public void verify_user_can_cancel_poll(){
+
+        ////button[@id='blog-submit-button-cancel']
+
+
+
+
+
+    }
+
+
+
+
+}
